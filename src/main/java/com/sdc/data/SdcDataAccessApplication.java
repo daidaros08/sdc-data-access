4909bf6839913ed1de08eb11263d28b3db87c7db
sebastian <sebastian.cabrera@valtech.com>
2024-12-17 17:09:31 -0300

    first commit

---
+++ src/main/java/com/sdc/data/SdcDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.sdc.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcDataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdcDataAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdc/data/SdcDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdc.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
