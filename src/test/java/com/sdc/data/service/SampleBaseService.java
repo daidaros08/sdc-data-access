1528779d9ab3844f143670043c33ed2e07a4fb1b
sebastian <sebastian.cabrera@valtech.com>
2024-12-19 19:44:59 -0300

    use envar

--- src/test/java/com/sdc/data/service/SampleBaseService.java
+++ src/test/java/com/sdc/data/service/SampleBaseService.java
@@ -1,11 +1,8 @@
 package com.sdc.data.service;
 
 import javax.validation.Validation;
-import javax.xml.validation.Validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import com.sdc.data.model.SampleEntity;
 import com.sdc.data.repository.SampleRepository;
 
